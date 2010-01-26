3925c93fea98550799c1aeea31a3e0cbdbb08250
Jeff Coleman <progressions@gmail.com>
2010-01-25 19:03:07 -0600

    Fixed file path

--- lib/epic.rb
+++ lib/epic.rb
@@ -1,5 +1,8 @@
+dir = File.dirname(__FILE__)
+$LOAD_PATH.unshift dir unless $LOAD_PATH.include?(dir)
+
 require 'rubygems'
-require 'lib/file'
+require 'file'
 require 'g'
 require 'active_support'
 require 'w3c_validators'
